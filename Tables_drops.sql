2c45a284a0dc6156be8140072f5125f5e5abe14e
Nigu652 <nicolas-setien@hotmail.com>
2023-05-26 20:13:26 -0400

    Tables_drops.sql modified

---
+++ Tables_drops.sql
@@ -0,0 +1,42 @@
+DROP TABLE historico_promocion;
+DROP TABLE entrega;
+DROP TABLE promocion;
+DROP TABLE denuncia;
+DROP TABLE mantenimiento_vehiculo;
+DROP TABLE mantenimiento_taller;
+DROP TABLE mantenimiento;
+DROP TABLE tipo_mantenimiento;
+DROP TABLE status_mantenimiento;
+DROP TABLE taller;
+DROP TABLE alianza;
+DROP TABLE aliado;
+DROP TABLE empleado;
+DROP TABLE ingreso;
+DROP TABLE gasto;
+DROP TABLE tipo_gasto;
+DROP TABLE detalle_pago;
+DROP TABLE forma_pago;
+DROP TABLE observacion;
+DROP TABLE rating;
+DROP TABLE detalle_alquiler;
+DROP TABLE alquiler CASCADE CONSTRAINTS;
+DROP TABLE reserva;
+DROP TABLE "DETALLE-COMPRA" CASCADE CONSTRAINTS;
+DROP TABLE compra CASCADE CONSTRAINTS;
+DROP TABLE vehiculo;
+DROP TABLE color;
+DROP TABLE tipo_vehiculo;
+DROP TABLE modelo;
+DROP TABLE marca;
+DROP TABLE status_vehiculo;
+DROP TABLE sede;
+DROP TABLE consesionario;
+DROP TABLE cliente CASCADE CONSTRAINTS;
+DROP TABLE tipo_cliente;
+DROP TABLE lugar CASCADE CONSTRAINTS;
+drop type informacion_personal;
+/
+drop type ubicacion_geografica;
+/
+drop type periodo_duracion;
+/
