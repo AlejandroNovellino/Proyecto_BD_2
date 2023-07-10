9880fbcd044fa016749233d0c53c31d201d4c392
Alejandro Novellino <alnovellino07@gmail.com>
2023-07-10 12:02:18 -0400

    report 13 without params finisehd

--- Funciones.sql
+++ Funciones.sql
@@ -291,7 +291,7 @@ End BUSCAR_Formas_Pago;
             -- todos los parametros son null
             open o_result_set for 
                 select tp_veh.tv_nombre tipo_vehiculo_r, marca.ma_nombre marca_vehiculo, modelo.m_nombre modelo_vehiculo, veh.v_anno anno_vehiculo, veh.v_placa placa_vehiculo, veh.v_foto foto_vehiculo, ra.r_escala_satisfaccion escala, (
-                    select XMLAGG(XMLELEMENT(E,obs.o_descripcion||'%n')).EXTRACT('//text()').getstringval()
+                    select XMLAGG(XMLELEMENT(E,obs.o_descripcion||' ')).EXTRACT('//text()').getstringval()
                     from observacion obs
                     where   obs.rating_r_id = ra.r_id
                 ) observaciones
@@ -305,26 +305,40 @@ End BUSCAR_Formas_Pago;
             ;
         else 
             -- no todos los parametros son null
+            -- funciona de forma que en lo que se cumpla alguna de las codicones ya regresa, es decir
+                -- si se desea un Kia, Sportage, AG25LK65 en lo que se cumpla una de estas ya traera al vehiculo,
+                -- usa or no and, para que obligue a que se cumplan todas las condiciones
             open o_result_set for 
                 select tp_veh.tv_nombre tipo_vehiculo_r, marca.ma_nombre marca_vehiculo, modelo.m_nombre modelo_vehiculo, veh.v_anno anno_vehiculo, veh.v_placa placa_vehiculo, veh.v_foto foto_vehiculo, ra.r_escala_satisfaccion escala, (
-                    select XMLAGG(XMLELEMENT(E,obs.o_descripcion||'%n')).EXTRACT('//text()').getstringval()
+                    select XMLAGG(XMLELEMENT(E,obs.o_descripcion||' ')).EXTRACT('//text()').getstringval()
                     from observacion obs
                     where   obs.rating_r_id = ra.r_id
                 ) observaciones
-                from alquiler alq, detalle_alquiler det_alq, vehiculo veh, tipo_vehiculo tp_veh, marca, modelo, rating ra
+                from (
+                        select *
+                        from alquiler a_aux
+                        where   fecha_inicio <= a_aux.a_periodo_duracion.P_Fecha_Inicio   or 
+                                fecha_fin  >= a_aux.a_periodo_duracion.P_Fecha_Fin
+                    ) alq, 
+                    (
+                        select *
+                        from vehiculo
+                        where   tipo_vehiculo_tv_id = (select tv_id from tipo_vehiculo where tv_nombre = tipo_vehiculo_param)   or
+                                modelo_marca_ma_id = (select ma_id from marca where ma_nombre = marca_param)                    or
+                                modelo_m_id = (select m_id from modelo where m_nombre = modelo_param)                           or
+                                v_placa = placa_param
+                    ) veh,
+                    detalle_alquiler det_alq, 
+                    rating ra,
+                    tipo_vehiculo tp_veh,
+                    marca, 
+                    modelo
                 where   alq.detalle_alquiler_da_id = det_alq.da_id  and
                         det_alq.vehiculo_v_placa = veh.v_placa      and
                         veh.tipo_vehiculo_tv_id = tp_veh.tv_id      and
                         veh.modelo_m_id = modelo.m_id               and
                         veh.modelo_marca_ma_id = marca.ma_id        and
-                        ra.alquiler_a_id = alq.a_id                 or
-                        -- elementos que dependen de los parametros
-                        fecha_inicio <= alq.a_periodo_duracion.P_Fecha_Inicio   or 
-                        fecha_fin  >= alq.a_periodo_duracion.P_Fecha_Fin        or
-                        tp_veh.tv_nombre = tipo_vehiculo_param                  or
-                        marca.ma_nombre = marca_param                           or
-                        modelo.m_nombre = modelo_param                          or
-                        placa_param = placa_param
+                        ra.alquiler_a_id = alq.a_id
             ;
         end if;
     end satisfaccion_del_cliente;
@@ -341,47 +355,139 @@ End BUSCAR_Formas_Pago;
         fecha_sabado date := fecha_inicio_semana + 5;
         fecha_domingo date := fecha_fin_semana;
     begin
-        -- Realizamos el query
-        -- es necesario que la data retorne de la forma (cant, dia) para tener una serie de cantidades y de dias
-        -- se puede hacer de dos formas:
-            -- se hace que la fecha inicio sea el inico de una semana y la fecha de fin seria el fin de la semana (esta es la opcion que se trabajara)
-            -- se cuentan los dias de la semana entre las fechas dadas
-        open o_result_set for 
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_lunes, 'DAY') dia FROM dual) dia_semana
-                where fecha_lunes between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-            union all
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_martes, 'DAY') dia FROM dual) dia_semana
-                where fecha_martes between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-            union all
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_miercoles, 'DAY') dia FROM dual) dia_semana
-                where fecha_miercoles between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-            union all
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_jueves, 'DAY') dia FROM dual) dia_semana
-                where fecha_jueves between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-            union all
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_viernes, 'DAY') dia FROM dual) dia_semana
-                where fecha_viernes between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-            union all
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_sabado, 'DAY') dia FROM dual) dia_semana
-                where fecha_sabado between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-            union all
-            select count(alq.a_id) cantidades,  dia_semana.dia 
-                from alquiler alq, (select to_char(fecha_domingo, 'DAY') dia FROM dual) dia_semana
-                where fecha_domingo between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
-                group by dia_semana.dia
-        ;
+        -- Verificamos si se dieron todos los parametros para el query
+        if ((fecha_inicio_semana is not NULL) and (fecha_fin_semana is not NULL)) then
+            -- Realizamos el query
+            -- es necesario que la data retorne de la forma (cant, dia) para tener una serie de cantidades y de dias
+            -- se puede hacer de dos formas:
+                -- se hace que la fecha inicio sea el inico de una semana y la fecha de fin seria el fin de la semana (esta es la opcion que se trabajara)
+                -- se cuentan los dias de la semana entre las fechas dadas
+            open o_result_set for 
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_lunes, 'DAY') dia FROM dual) dia_semana
+                    where fecha_lunes between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+                union all
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_martes, 'DAY') dia FROM dual) dia_semana
+                    where fecha_martes between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+                union all
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_miercoles, 'DAY') dia FROM dual) dia_semana
+                    where fecha_miercoles between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+                union all
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_jueves, 'DAY') dia FROM dual) dia_semana
+                    where fecha_jueves between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+                union all
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_viernes, 'DAY') dia FROM dual) dia_semana
+                    where fecha_viernes between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+                union all
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_sabado, 'DAY') dia FROM dual) dia_semana
+                    where fecha_sabado between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+                union all
+                select count(alq.a_id) cantidades,  dia_semana.dia 
+                    from alquiler alq, (select to_char(fecha_domingo, 'DAY') dia FROM dual) dia_semana
+                    where fecha_domingo between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin
+                    group by dia_semana.dia
+            ;
+        else 
+            -- Como no se dieron los parametros entonces se busca para todos los dias de la semana
+            -- del anno
+            open o_result_set for
+                -- Query para los lunes
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('19/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+                union all
+                -- Query para los martes
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('20/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+                union all
+                -- Query para los miercoles
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('21/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+                union all
+                -- Query para los jueves
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('22/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+                union all
+                -- Query para los viernes
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('23/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+                union all
+                -- Query para los sabado
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('24/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+                union all
+                -- Query para los domingo
+                select count(alq.a_id) cantidades, to_char(dias.dia, 'Day') dia
+                from    (
+                            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+                            from DUAL 
+                            connect by rownum < 366
+                        ) dias,
+                        alquiler alq
+                where   to_char(dias.dia, 'Day') like to_char(to_date('25/06/2023','dd/mm/yyyy'), 'Day') and
+                        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+                group by to_char(dias.dia, 'Day')
+            ;
+        
+        end if;
     end cantidad_alquileres_por_dia_de_semana;
 
   End Pk_Alquiler;
--- REPORT_13_TEST.sql
+++ REPORT_13_TEST.sql
@@ -15,3 +15,136 @@ FROM
     
 select alq.a_periodo_duracion.P_Fecha_Inicio, alq.a_periodo_duracion.P_Fecha_Fin
     from alquiler alq;
+    
+select tp_veh.tv_nombre tipo_vehiculo_r, marca.ma_nombre marca_vehiculo, modelo.m_nombre modelo_vehiculo, veh.v_anno anno_vehiculo, veh.v_placa placa_vehiculo, veh.v_foto foto_vehiculo, ra.r_escala_satisfaccion escala, (
+                    select XMLAGG(XMLELEMENT(E,obs.o_descripcion||' ')).EXTRACT('//text()').getstringval()
+                    from observacion obs
+                    where   obs.rating_r_id = ra.r_id
+                ) observaciones
+                from (
+                        select *
+                        from alquiler a_aux
+                        where   TO_DATE('06/01/2023','MM/DD/YYYY') <= a_aux.a_periodo_duracion.P_Fecha_Inicio   or 
+                                TO_DATE('07/30/2023','MM/DD/YYYY')  >= a_aux.a_periodo_duracion.P_Fecha_Fin
+                    ) alq, 
+                    (
+                        select *
+                        from vehiculo
+                        where   tipo_vehiculo_tv_id = (select tv_id from tipo_vehiculo where tv_nombre = 'Carro')     or
+                                modelo_marca_ma_id = (select ma_id from marca where ma_nombre = 'Kia')                or
+                                modelo_m_id = (select m_id from modelo where m_nombre = 'Sportage')                   or
+                                v_placa = NULL
+                    ) veh,
+                    detalle_alquiler det_alq, 
+                    rating ra,
+                    tipo_vehiculo tp_veh,
+                    marca, 
+                    modelo
+                where   alq.detalle_alquiler_da_id = det_alq.da_id  and
+                        det_alq.vehiculo_v_placa = veh.v_placa      and
+                        veh.tipo_vehiculo_tv_id = tp_veh.tv_id      and
+                        veh.modelo_m_id = modelo.m_id               and
+                        veh.modelo_marca_ma_id = marca.ma_id        and
+                        ra.alquiler_a_id = alq.a_id                               
+                       -- elementos que dependen de los parametros
+            ;
+      
+SELECT TRUNC (to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - ROWNUM) dt
+  FROM DUAL CONNECT BY ROWNUM < 366;
+
+-- Query de prueba
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('19/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day');
+
+-- Actualmente pareciera que funciona
+-- Query para los lunes
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('19/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day')
+union all
+-- Query para los martes
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('20/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day')
+union all
+-- Query para los miercoles
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('21/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day')
+union all
+-- Query para los jueves
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('22/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day')
+union all
+-- Query para los viernes
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('23/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day')
+union all
+-- Query para los sabado
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('24/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day')
+union all
+-- Query para los domingo
+select count(alq.a_id), to_char(dias.dia, 'Day') dia_semana
+from    (
+            select trunc(to_date(concat('01/01/',to_char(extract(year from SYSDATE)+1)),'dd/mm/yyyy') - rownum) dia
+            from DUAL 
+            connect by rownum < 366
+        ) dias,
+        alquiler alq
+where   to_char(dias.dia, 'Day') like to_char(to_date('25/06/2023','dd/mm/yyyy'), 'Day') and
+        dias.dia between alq.a_periodo_duracion.P_Fecha_Inicio and alq.a_periodo_duracion.P_Fecha_Fin   
+group by to_char(dias.dia, 'Day');
